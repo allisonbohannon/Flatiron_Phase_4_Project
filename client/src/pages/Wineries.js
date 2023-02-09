0e56ba8b3fef891b6ee65a5ccf6753f250ab5f03
Allison Bohannon <akboh.github@gmail.com>
2023-02-09 05:36:56 -0500

    Base setup for Nav and Routes

---
+++ client/src/pages/Wineries.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Wineries extends Component {
+  render() {
+    return (
+      <div>Wineries</div>
+    )
+  }
+}
+
+export default Wineries
