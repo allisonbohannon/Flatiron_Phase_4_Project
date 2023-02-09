133356be9537f85738df6a10ada68ba0f074813d
Allison Bohannon <akboh.github@gmail.com>
2023-02-09 06:01:55 -0500

    Install react bootstrap in client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,40 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import { useState, useEffect } from "react";
-import NavigationBar from "./NavigationBar";
-import Wineries from "../pages/Wineries";
-import Users from "../pages/Users";
-
-
 
 
 function App() {
-
-  //useState for wineries and users 
-  //useEffect to set initial states 
-
-
   return (
-     <div>
-            <NavigationBar/>
-            <Routes>
-                {/* <Route path="/read" element={<CompletedReads
-                    bookList={bookList}
-                    handleUpdate={onUpdate}
-                    handleDelete={onDelete}
-                />} />
-                 <Route path="/unread" element={<CompletedReads
-                    bookList={bookList}
-                    handleUpdate={onUpdate}
-                    handleDelete={onDelete}
-                />} />
-                <Route path="/" element={<Home
-                    bookList={bookList}
-                    handleAdd={onAdd}
-                    handleUpdate={onUpdate}
-                    handleDelete={onDelete}
-                />} /> */}
-            </Routes>
-        </div>
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello, World
+        </a>
+      </header>
+    </div>
   );
 }
 
