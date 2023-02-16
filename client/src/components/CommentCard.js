7804c8fa834647df10d2058fac52ca7d796ace1c
Allison Bohannon <akboh.github@gmail.com>
2023-02-15 21:41:44 -0800

    Add context to comments

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../context/User';
+import { Button, Card } from '../styles';
 
 
 const CommentCard = ({comment, winery}) => {
 
     const currentUser = useContext(UserContext)
 
+    console.log(currentUser)
 
-    const checkIfCurrentUser = currentUser === comment.userId ? true : false 
+
+    const checkIfCurrentUser = currentUser.currentUser === comment.userId ? true : false 
         
 
     const handleEditComment = {
@@ -21,17 +22,17 @@ const CommentCard = ({comment, winery}) => {
     } 
 
     
-    const author = <Card.Subtitle>{comment.userId}</Card.Subtitle>
+    const author = <p>{comment.userId}</p>
     const button = <Link to={`/wineries/${winery.id}/comments/${comment.id}/edit`}>
                     <Button onClick={handleEditComment}>Edit</Button>
                     </Link>
 
   return (
-    <Card>
-        <Card.Text>{comment.text}</Card.Text>
+    <div>
+        <p>{comment.text}</p>
         {checkIfCurrentUser ? button : author}
 
-    </Card>
+    </div>
   )
 }
 
--- client/src/pages/WineryDetail.js
+++ client/src/pages/WineryDetail.js
@@ -11,8 +11,11 @@ import { UserContext } from '../context/User';
 const WineryDetail = ({wineries, onChangeRating, onAddRating}) => {
 
   const { wineryId } = useParams()
-  const currentUser = useContext(UserContext)
+  const {currentUser, setCurrentUser} = useContext(UserContext)
   const navigate = useNavigate()
+
+  //delete me!
+  setCurrentUser(1)
 
   const displayWinery = wineries.find(winery => winery.id === parseInt(wineryId))
 
