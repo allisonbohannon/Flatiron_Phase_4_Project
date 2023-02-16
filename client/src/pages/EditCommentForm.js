6d75f3550f18d7ebff6f6bc6ebddb847af7c0aad
Allison Bohannon <akboh.github@gmail.com>
2023-02-16 09:13:28 -0800

    Add styling to edit form

--- client/src/pages/EditCommentForm.js
+++ client/src/pages/EditCommentForm.js
@@ -1,31 +1,32 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { UserContext } from '../context/User';
-import { CardContainer, FormField, Button } from '../styles';
+import { Container, FormField, Button, CardHeading } from '../styles';
 
 
-const EditCommentForm = ({wineries, handleCommentEdit}) => {
+const EditCommentForm = ({wineries, onCommentEdit, onCommentDelete}) => {
 
     const navigate = useNavigate()
 
-    const currentUser = useContext(UserContext)
+    const {currentUser} = useContext(UserContext)
 
     const {wineryId, commentId} = useParams()
 
     const winery = wineries.find(winery => winery.id === parseInt(wineryId))
     const comment = winery.comments.find(comment => comment.id === parseInt(commentId))
+    
 
     
-    useEffect(() => {
-        if (comment.userId !== currentUser) {
-            navigate(`/wineries/${winery.id}/comments/${comment.id}`)
-            console.log("Edit Access Denied")
-        }
-    })
+    // useEffect(() => {
+    //     if (comment.userId !== currentUser) {
+    //         navigate(`/wineries/${winery.id}/comments/${comment.id}`)
+    //         console.log("Edit Access Denied")
+    //     }
+    // })
 
     const [commentText, setCommentText] = useState(comment.text)
   
-    const handleSubmit = (e) => {
+    const handleEditComment = (e) => {
         e.preventDefault()
 
         const updatedComment = {...comment, text: commentText}
@@ -38,6 +39,7 @@ const EditCommentForm = ({wineries, handleCommentEdit}) => {
             }
         })}
 
+
         // fetch(`${process.env.REACT_APP_API_URL}/comments/${id}`, { 
         //     method: "PATCH", 
         //     headers: { "Content-Type": "application/json" },
@@ -46,29 +48,50 @@ const EditCommentForm = ({wineries, handleCommentEdit}) => {
         //  .then(response => response.json())
         //  .then(data => handleCommentEdit(data))
 
-        handleCommentEdit(updatedWinery)
+        onCommentEdit(updatedWinery)
 
-       // navigate(-1)
+        navigate(-1)
     }
 
-    const handleChange = (e) => {
-        setCommentText(e.target.value)
+    const handleDelete = () => {
+
+        const updatedWinery = {...winery, comments: winery.comments.filter(originalComment => {
+            if (comment.id !== originalComment.id) {
+                return true
+            } else {
+                return false
+            }
+        })}
+
+        onCommentDelete(updatedWinery)
+
+        navigate(-1)
+        
     }
     
   
    
   return (
-    <CardContainer>
-        <form c onSubmit={handleSubmit}>
-                <FormField type="text" 
-                        name="name"
-                        onChange={handleChange} 
-                        value={commentText}>
-                </FormField> 
-                <Button type="submit">Submit</Button>
-        </form>
-      
-    </CardContainer>
+    <Container>
+      <CardHeading>Tell us what you think about {winery.name}! </CardHeading>
+      <br></br>
+      <form onSubmit={handleEditComment}>
+        <FormField>
+          <textarea
+            type="textarea"
+            id="comment"
+            autoComplete="off"
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+            style={{height:'15em', width:'30em', borderRadius:'6px'}}
+          ></textarea>
+        </FormField>
+        <FormField>
+          <Button variant="fill" color="primary" type="submit">Submit</Button>
+          <Button variant="fill" color="primary" onClick={handleDelete}>Delete</Button>
+        </FormField>
+      </form>
+    </Container>
   )
 }
 
